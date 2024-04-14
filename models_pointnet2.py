063faf81af79ff013ee537216c6a4928e646c455
Madhu Korada <manoj.korada0000@gmail.com>
2024-04-14 01:48:34 +0000

    Remove commented out code and unused function

--- models_pointnet2.py
+++ models_pointnet2.py
@@ -5,14 +5,6 @@ from time import time
 import numpy as np
 
 
-# def pc_normalize(pc):
-#     l = pc.shape[0]
-#     centroid = np.mean(pc, axis=0)
-#     pc = pc - centroid
-#     m = np.max(np.sqrt(np.sum(pc**2, axis=1)))
-#     pc = pc / m
-#     return pc
-
 def square_distance(src, dst):
     """
     Calculate Euclid distance between each two points.
